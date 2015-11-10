4ae32dcd58065bc50d69e7b0ea877d39804e39fb
lucssmmassuh <lucas.massuh@gmail.com>
2015-11-10 18:46:51 -0300

    making search placeholder random, before it was always the same shit with different smell

--- www/js/Translations.js
+++ www/js/Translations.js
@@ -6,12 +6,18 @@
       //English
       translationsEN: {
         HEADLINE: 'HI!',
+        SEARCH_PLACEHOLDER1: "sushi restaurant, tomato stain remover",
+        SEARCH_PLACEHOLDER2: "muscle car, icecream flavor, beach ",
+        SEARCH_PLACEHOLDER3: "date for getting married, TV show",
+        SEARCH_PLACEHOLDER4: "book for kids, disney movie, candy",
+        SEARCH_PLACEHOLDER5: "childhood game, jeans brand",
         SEARCH: 'Search',
         GO:'Search',
         ANSWER_THIS: 'Answer this while we get you to The Best',
         WHAT_IS_THE_BEST: 'What is The Best',
         SKIP: 'Skip',
-        ANSWER: "Responder",
+        ANSWER: "Answer",
+        THE_ANSWER_IS: "The answer:",
         AGREE:"Agree!",
         ASK_AGAIN:"Ask Again",
         NAH:"Nah...",
@@ -20,33 +26,41 @@
         YOU_ARE_FIRST:"you are the first to ask for The Best",
         VOTE_BEST:"Vote your best",
         ASK_FRIENDS:"Ask your friends",
-        IS:"is:",
+        IS:"is",
         SHARE: "Share",
+        HELP_OTHERS: "Now, help others find ",
         MAIN : {
-          TITLE: 'The Best:'
+          TITLE: 'The Best'
         }
       },
       //Spanish
       translationsES: {
         HEADLINE: 'HOLA!',
-        SEARCH: 'Buscar',
-        GO:'Buscar',
-        ANSWER_THIS: 'Responde ésto mientras buscamos The Best:',
+        SEARCH_PLACEHOLDER1: "actor, celular, quita esmalte",
+        SEARCH_PLACEHOLDER2: "auto deportivo, sabor de helado, playa",
+        SEARCH_PLACEHOLDER3: "fecha para casarse, programa de TV",
+        SEARCH_PLACEHOLDER4: "libro de cuentos, película, caramelo",
+        SEARCH_PLACEHOLDER5: "juego de la infancia, marca de jean",
+        SEARCH: "Buscar",
+        GO:"Buscar",
+        ANSWER_THIS: "Responde ésto mientras buscamos The Best:",
         WHAT_IS_THE_BEST: 'Cuál es The Best',
-        SKIP: 'Omitir',
+        SKIP: "Omitir",
         ANSWER: "Responder",
-        AGREE:"Si !!",
+        THE_ANSWER_IS: "Respuesta:",
+        AGREE:"Si!",
         ASK_AGAIN:"Preguntar nuevamente",
         NAH:"Nah...",
-        THANKS:"Gracias!! Tu eres The Best!",
-        GOODONE:"Buenísimo!",
+        THANKS:"Genial! Tu eres The Best!",
+        GOODONE:"Excelente!",
         YOU_ARE_FIRST:"Eres el primero en preguntar por The Best",
         VOTE_BEST:"Vota por tu The Best",
         ASK_FRIENDS:"Pregunta a tus amigos",
-        IS:"es:",
+        IS:"es",
         SHARE: "Compartir",
+        HELP_OTHERS: "Ahora, ayuda a otros a encontrar",
         MAIN : {
-          TITLE: 'The Best:'
+          TITLE: 'The Best'
         }
       }
     });
