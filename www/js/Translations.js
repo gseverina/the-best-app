655aab7aac8cd99e7acf92b1a7dde5f9eccdd556
lucssmmassuh <lucas.massuh@gmail.com>
2015-11-13 13:56:10 -0300

    updating new logo and translation for better fit on answer page, before it was longer than the line at ANSES on a monday morning.

--- www/js/Translations.js
+++ www/js/Translations.js
@@ -4,7 +4,7 @@
   angular.module('Translations', [])
     .constant('Translations', {
       //English
-      translationsEN: {
+      translationsES: {
         HEADLINE: 'HI!',
         SEARCH_PLACEHOLDER1: "sushi restaurant, tomato stain remover",
         SEARCH_PLACEHOLDER2: "muscle car, icecream flavor, beach ",
@@ -34,7 +34,7 @@
         }
       },
       //Spanish
-      translationsES: {
+      translationsEN: {
         HEADLINE: 'HOLA!',
         SEARCH_PLACEHOLDER1: "actor, celular, quita esmalte",
         SEARCH_PLACEHOLDER2: "auto deportivo, sabor de helado, playa",
